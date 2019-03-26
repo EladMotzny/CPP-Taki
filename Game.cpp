68ae41e464fd515aaa1a194824cc6a1119ce723d
alex <alexyafish@gmail.com>
2019-03-26 22:40:23 +0200

    started working on Game

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,16 @@
 #include "Game.h"
+#include <iostream> 
+#include <deque> 
+
+using namespace std;
 
 void Game::start(){
     printf("erjgejrgnfng");
+}
+
+Game::Game(int numberOfPlayers, string nameArray[], int numberOfCards){
+    for (int i=0; i<sizeof(nameArray); i++) {
+        Player* p = new Player(nameArray[i], numberOfCards);
+        this->playersDeque.push_back(*p);
+    }
 }
--- Game.h
+++ Game.h
@@ -3,15 +3,20 @@
 #define GAME_H
 #include "Player.h"
 #include "Card.h"
+#include <deque> 
+using namespace std;
 class Game {
 private:
 	//private members and functions
-	int numberOfPlayers;
+	deque <Player> playersDeque;
 	int numberOfCards;
-	std::string playersNameArray[];
+	Card currentCard;
+	Player currentPlayer;
+	bool clockWise = true;
 public:
 	//public members and functions
 	Game(){}
+	Game(int numberOfPlayers, string nameArray [], int numberOfCards);
 	void start();
 };
 #endif
