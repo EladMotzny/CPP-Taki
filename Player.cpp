dace0375dd3cd62a7f06252a0539b012ed41a76d
Elad Motzny <emotzny@gmail.com>
2019-03-26 22:00:33 +0200

    Added decleration for functions

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,11 @@
 #include "Player.h"
+
+Player::Player(){}
+
+Player::Player(string name, int numOfCards){}//regular constractor
+
+Player::Player(const Player& pl){}//copy constractor
+
+Player::~Player(){};//destructor
+
+bool Player::play(const Card& c){}//the play function
--- Player.h
+++ Player.h
@@ -1,14 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Card.h"
+using namespace std;
+
 class Player {
 private:
 	// name;
+	string name;
 	// num_of_cards;
+	int num_cards;
 	// container of cards
+	vector <Card> playerCards;
 	// more private members
 
 public:
+
+Player();//empty constructor
+Player(string name, int numOfCards);//regular constractor
+Player(const Player& pl);//copy constractor
+~Player();//destructor
+bool play(const Card& c);//the play function
+
 
 };
 #endif
