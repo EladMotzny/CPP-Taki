ef8d7bb1507dfaf6bd67dce23ee9d88441e9238f
alex <alexyafish@gmail.com>
2019-03-26 23:40:57 +0200

    initialized the Game so far

--- Game.cpp
+++ Game.cpp
@@ -6,13 +6,38 @@
 
 using namespace std;
 
-void Game::start(){
-    printf("erjgejrgnfng");
-}
 
 Game::Game(int numberOfPlayers, vector<string> nameArray, int numberOfCards){
     for (int i=0; i<nameArray.size(); i++) {
         Player* p = new Player(nameArray[i], numberOfCards);
         this->playersDeque.push_back(*p);
     }
+}
+
+void Game::start(){
+    deque <Player> playersDeque;
+	int numberOfCards;
+    int numberOfPlayers;
+    vector<string> nameArray;
+	Card currentCard;
+	Player currentPlayer;
+
+    cout << "How many players?" << endl;
+    cin >> numberOfPlayers;
+
+    cout << "How many cards?" << endl;
+    cin >> numberOfCards;
+
+    for(int i=1; i<=numberOfPlayers; i++){
+        string currentName;
+        cout << "Player number " << i << " name?"<< endl;
+        cin >> currentName;
+        nameArray.push_back(currentName);
+    }
+    Game(numberOfPlayers, nameArray, numberOfCards);
+	// bool clockWise = true
+}
+
+void gamePrintForPlayer(){
+
 }
