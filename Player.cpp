05d0c8e39dc24b710129a930d109c4bf3e2c0172
Elad Motzny <emotzny@gmail.com>
2019-03-27 21:23:34 +0200

    Added constructors to Player

--- Player.cpp
+++ Player.cpp
@@ -1,14 +1,29 @@
 #include "Player.h"
 
-Player::Player(){}
+Player::Player(){//empty constructor
+    this->name = "Jahlawit";
+    this->num_cards = 0;
+}
 
-Player::Player(string name, int numOfCards){}//regular constractor
+Player::Player(string name, int numOfCards){//regular constructor
+    this->name = name;
+    this->num_cards = numOfCards;
 
-Player::Player(const Player& pl){}//copy constractor
+}
+
+Player::Player(const Player& pl){// copy constructor
+    this->name = pl.name;
+    this->num_cards = pl.num_cards;
+    this->playerCards = pl.playerCards;
+
+
+}
 
 Player::~Player(){};//destructor
 
-bool Player::play(const Card& c){}//the play function
+bool Player::play(const Card& c){//The play function
+    return true;
+}//NEED TO CHANGE
 
 string Player::getName(){
     return this->name;
